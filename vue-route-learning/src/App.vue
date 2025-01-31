24ed45b1425c9fb98a289b4b09099c038a8b27e9
Gabriel Ramos <whosramoss@gmail.com>
2025-01-31 16:35:19 -0300

    chore: add vue-route-learning

---
+++ vue-route-learning/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { RouterView } from 'vue-router';
+
+import Nav from './components/Nav.vue';
+</script>
+
+<template>
+  <div>
+    <Nav />
+    <RouterView />
+  </div>
+</template>
