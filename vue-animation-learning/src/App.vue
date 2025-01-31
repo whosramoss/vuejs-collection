1b5b5889d357205b452394d74de809254c93adc9
Gabriel Ramos <whosramoss@gmail.com>
2025-01-31 16:26:59 -0300

    chore: add vue-animation-learning

---
+++ vue-animation-learning/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { RouterView, RouterLink } from 'vue-router';
+</script>
+
+<template>
+  <header class="container">
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/about">About</RouterLink>
+  </header>
+  <RouterView />
+</template>
+
+<style scoped>
+.container {
+  max-width: 300px;
+  margin: 0 auto;
+}
+</style>
