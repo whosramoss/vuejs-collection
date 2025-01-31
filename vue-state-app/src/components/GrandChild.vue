ed5a869088ca7bbb61815c85425f6db7f30132cb
Gabriel Ramos <whosramoss@gmail.com>
2025-01-31 16:35:58 -0300

    chore: add vue-state-app

---
+++ vue-state-app/src/components/GrandChild.vue
@@ -0,0 +1,11 @@
+<script setup>
+import GreatGrandChild from './GreatGrandChild.vue';
+</script>
+
+<template>
+  <div>
+    <h1>Grand Child</h1>
+    <div class="line"></div>
+    <GreatGrandChild />
+  </div>
+</template>
